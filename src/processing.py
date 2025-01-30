b5248d6e44ab292528006aa7d347c433a7af6d0d
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-01-30 22:26:41 +0300

    add function sort by date and check result

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def filter_by_state(data_dict: list[dict], state='EXECUTED')->list[dict]:
     new_data_list = []
     for item in data_dict:
@@ -5,4 +7,22 @@ def filter_by_state(data_dict: list[dict], state='EXECUTED')->list[dict]:
             new_data_list.append(item)
     return new_data_list
 
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+def sort_by_date(data_dict: list[dict], sort_sequence: bool = True)-> list[dict]:
+    new_date_list = []
+    for item in data_dict:
+        if 'date' in item.keys():
+            new_date_list.append(item)
+
+    sorted_of_date_list = sorted(
+        new_date_list,
+        key=lambda x: datetime.strptime(x.get("date"), "%Y-%m-%dT%H:%M:%S.%f"),
+        reverse=sort_sequence,
+    )
+
+    return sorted_of_date_list
+
+
+
+
+
+
