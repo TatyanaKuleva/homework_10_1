2f61ae3215b16aca01290047a5c1e3b21c12a2a3
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-01-30 20:45:27 +0300

    add function filtr by state

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(data_dict: list[dict], state='EXECUTED')->list[dict]:
+    new_data_list = []
+    for item in data_dict:
+        if item['state'] == state:
+            new_data_list.append(item)
+    return new_data_list
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
