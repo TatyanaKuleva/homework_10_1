eb7e9ccc990f666fd8459ad958eb4122a72e1bd2
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-02-07 21:12:26 +0300

    add html and fixed issues from linter

--- src/processing.py
+++ src/processing.py
@@ -2,36 +2,28 @@ def filter_by_state(data_dict: list[dict], state: str = "EXECUTED") -> list[dict
     """фильтрует список словарей в соответствии с выбранным статусом"""
     new_data_list = []
     for item in data_dict:
-        if 'state' in item.keys():
-            if item['state'] == state:
+        if "state" in item.keys():
+            if item["state"] == state:
                 new_data_list.append(item)
             if len(new_data_list) == 0:
-                raise ValueError(
-                    'Нет данных для указанного типа статуса'
-                )
+                raise ValueError("Нет данных для указанного типа статуса")
         else:
-            raise ValueError(
-                'отсутствует статус для фильтрации'
-            )
-
+            raise ValueError("отсутствует статус для фильтрации")
 
     return new_data_list
+
 
 def sort_by_date(data_dict: list[dict], sort_sequence: bool = True) -> list[dict]:
     """сортирует список словарей по дате транзакции в соответствии с выбранным вариантом сортировки"""
     for item in data_dict:
-       if item['date'][0] == 'T' or 'T' not in item['date']:
-           raise ValueError(
-               'нет даты транзакции для сортировки'
-           )
+        if item["date"][0] == "T" or "T" not in item["date"]:
+            raise ValueError("нет даты транзакции для сортировки")
 
-    sorted_by_date_list = sorted(data_dict, key=lambda x: (x['date'], x['id']), reverse=sort_sequence)
+    sorted_by_date_list = sorted(data_dict, key=lambda x: (x["date"], x["id"]), reverse=sort_sequence)
     return sorted_by_date_list
-
 
 
 # def sort_by_date(data_dict: list[dict], sort_sequence: bool = True) -> list[dict]:
 #     """сортирует список словарей по дате транзакции в соответствии с выбранным вариантом сортировки"""
 #     sorted_by_date_list = sorted(data_dict, key=lambda x: (x['date'], x['id']), reverse=sort_sequence)
 #     return sorted_by_date_list
-
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,12 +5,14 @@ from src.masks import get_mask_account, get_mask_card_number
 def test_get_mask_card_number_right_result(standart_number_card, result_mask_number_card):
     assert get_mask_card_number(standart_number_card) == result_mask_number_card
 
-@pytest.mark.parametrize('get_number_card', [('123'), ('111111111111111111111111111111111'), (''), ('123455954')])
+
+@pytest.mark.parametrize("get_number_card", [("123"), ("111111111111111111111111111111111"), (""), ("123455954")])
 def test_get_mask_card_number_right_len(get_number_card):
     with pytest.raises(ValueError) as mis_len_number:
         get_mask_card_number(get_number_card)
 
-@pytest.mark.parametrize('get_number_card', [('7000792289FG6361'), ('asdkflfndls;dkfl')])
+
+@pytest.mark.parametrize("get_number_card", [("7000792289FG6361"), ("asdkflfndls;dkfl")])
 def test_get_mask_card_number_no_all_digit(get_number_card):
     with pytest.raises(ValueError) as no_digit_number:
         get_mask_card_number(get_number_card)
@@ -20,14 +22,15 @@ def test_get_mask_card_account_right_result(standart_number_account, result_mask
     assert get_mask_account(standart_number_account) == result_mask_number_account
 
 
-@pytest.mark.parametrize('get_number_account', [('123'), ('111111111111111111111111111111111'), (''), ('123455954')])
-def test_get_mask_card_number_right_len(get_number_account):
+@pytest.mark.parametrize("get_number_account", [("123"), ("111111111111111111111111111111111"), (""), ("123455954")])
+def test_get_mask_account_right_len(get_number_account):
     with pytest.raises(ValueError) as mis_len_account:
         get_mask_account(get_number_account)
 
 
-@pytest.mark.parametrize('get_number_account', [('7365410843013584D05'), ('asdkflfndls;dkfl'), ('7365410843013584 05')])
-def test_get_mask_card_number_no_all_digit(get_number_account):
+@pytest.mark.parametrize(
+    "get_number_account", [("7365410843013584D05"), ("asdkflfndls;dkfl"), ("7365410843013584 05")]
+)
+def test_get_mask_card_account_no_all_digit(get_number_account):
     with pytest.raises(ValueError) as no_digit_account:
         get_mask_account(get_number_account)
-
