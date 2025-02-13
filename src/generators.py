77e18b88708727dc1898ce7c243fbdc8672d702c
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-02-13 23:26:32 +0300

    fixed test generators

--- src/generators.py
+++ src/generators.py
@@ -1,8 +1,7 @@
-
 def filter_by_currency(list_transction, type_currence):
     "Фильтрует список транзакций в указаной валюте"
     if len(list_transction) == 0:
-        raise ValueError ('Нет данных для фильтрации')
+        raise ValueError("Нет данных для фильтрации")
     else:
         for transaction in list_transction:
             if transaction["operationAmount"]["currency"]["name"] == type_currence:
@@ -12,23 +11,17 @@ def filter_by_currency(list_transction, type_currence):
 def transaction_descriptions(list_transction):
     "принимает список транзакций и возвращает описание каждой операции"
     if len(list_transction) == 0:
-        raise ValueError ("Нет данных для вывода либо список транзакций пуст")
+        raise ValueError("Нет данных для вывода либо список транзакций пуст")
     else:
         for transaction in list_transction:
             yield transaction["description"]
 
+
 def card_number_generator(start, stop):
     "Генерирует номера карт в заданном диапазоне"
-    for i in range(start, stop+1):
-        card_number = f"{i:016}"
-        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
-
-
-
-
-
-
-
-
-
-
+    if start < 1 or stop > 9999999999999999:
+        raise ValueError("Вы ввели неверные данные для формирования номера")
+    else:
+        for i in range(start, stop + 1):
+            card_number = f"{i:016}"
+            yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
