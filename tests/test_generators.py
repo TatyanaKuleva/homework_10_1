1fbd6046b850beabfa0fb1bd333e847e5bf8b35f
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-02-13 20:29:21 +0300

    add tests function generators

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -6,6 +6,38 @@ def test_get_filter_by_currency_right_result(get_list_transaction, result_filtr_
     assert next(usd_transactions) == result_filtr_transaction
 
 
-def test_get_filter_by_currency_no_date_for_g(get_type_number_mis):
-    with pytest.raises(ValueError) as mis_len_type_number:
-        mask_account_card(get_type_number_mis)
+def test_get_filter_by_currency_no_date_for_filtr(get_list_transaction):
+    filtr_transaction= filter_by_currency(get_list_transaction, "EURO")
+    for _ in range(5):
+        try:
+            print(next(filtr_transaction))
+        except:
+            print("Нет данных по выбранной валюте")
+
+def test_get_filter_by_empty_dict(get_empty_list):
+    usd_transactions = filter_by_currency(get_empty_list, "USD")
+    with pytest.raises(ValueError) as empty_list:
+        assert next(usd_transactions)
+
+def test_transaction_descriptions_right_result(get_list_transaction):
+    descripton_res = transaction_descriptions(get_list_transaction)
+    assert next(descripton_res) == "Перевод организации"
+    assert next(descripton_res) == "Перевод со счета на счет"
+    assert next(descripton_res) == "Перевод со счета на счет"
+    assert next(descripton_res) == "Перевод с карты на карту"
+    assert next(descripton_res) == "Перевод организации"
+
+def test_transaction_descriptions_epmty_list(get_empty_list):
+    descripton_res_empty = transaction_descriptions(get_empty_list)
+    with pytest.raises(ValueError) as empty_list:
+        assert next(descripton_res_empty)
+
+@pytest.mark.parametrize(
+    "start, stop, expected_result",
+    [
+        ("1", "3", "0000 0000 0000 0001 0000 0000 0000 0002 0000 0000 0000 0003")
+    ],
+)
+def test_card_number_generator_right_result(start, stop, expected_result):
+    gen = card_number_generator(start, stop)
+    assert next(gen) == expected_result
