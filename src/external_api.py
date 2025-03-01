c8f2afedfc54cbc4c3eb8afc88ff49985c5bfa5f
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-03-01 12:56:58 +0300

    fixed second function

--- src/external_api.py
+++ src/external_api.py
@@ -1,21 +1,29 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-def convertion_currency(amount: float, currency: str) -> float:
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}&from={currency}&amount={amount}"
 
-    headers = {
-        "apikey": "zzZbE9mmkrkoa3MsaJPHZ4UprySc0MZB"
-    }
+def convertion_currency(amount: str, currency: str):
+    load_dotenv('.env')
+    API_KEY = os.getenv('API_KEY')
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+
+    headers = {"apikey": f"{API_KEY}"}
 
     response = requests.get(url, headers=headers)
 
-    result = response.json()['result']
 
-    return result
+    if response.status_code != 200:
+        return False
+
+    result = response.json()
+    rub_amount = float(result["result"])
+
+    return rub_amount
 
 
 if __name__== '__main__':
-    print(convertion_currency(30.0, "USD"))
+    print(convertion_currency("1.0", "USD"))
 
 
 
---
+++ tests/tests_external_api.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch, Mock
+import requests
+from src.external_api import convertion_currency
+
+
+# def get_github_user_info(username):
+#     response = requests.get(f'https://api.github.com/users/{username}')
+#     return response.json()
+
+@patch('requests.get')
+def test_get_convertion_currency(mock_get):
+    mock_get.return_value.json.return_value = {'login': 'testuser', 'name': 'Test User'}
+    assert get_github_user_info('testuser') == {'login': 'testuser', 'name': 'Test User'}
+    mock_get.assert_called_once_with('https://api.github.com/users/testuser')
