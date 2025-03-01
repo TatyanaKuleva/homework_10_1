db1cf7dc3771a3bfe144beadf912d2e80d83cb0b
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-03-02 00:35:27 +0300

    try to create test with mock and patch

--- src/external_api.py
+++ src/external_api.py
@@ -5,14 +5,13 @@ from dotenv import load_dotenv
 
 def convertion_currency(amount: str, currency: str):
     """функция конвертации валюты в рубли с помощью Exchange Rates Data API"""
-    load_dotenv('.env')
-    API_KEY = os.getenv('API_KEY')
+    load_dotenv(".env")
+    API_KEY = os.getenv("API_KEY")
     url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
 
     headers = {"apikey": f"{API_KEY}"}
 
     response = requests.get(url, headers=headers)
-
 
     if response.status_code != 200:
         return False
@@ -23,8 +22,5 @@ def convertion_currency(amount: str, currency: str):
     return rub_amount
 
 
-if __name__== '__main__':
+if __name__ == "__main__":
     print(convertion_currency("1.0", "USD"))
-
-
-
