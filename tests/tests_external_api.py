9c2ed319909b19b9e608890a18064ddaf4678a8b
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-03-01 16:05:47 +0300

    fixed second function part 2

--- tests/tests_external_api.py
+++ tests/tests_external_api.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch, Mock
 import requests
+from mypy.typeanal import analyze_type_alias
+
 from src.external_api import convertion_currency
 
 
@@ -7,8 +9,14 @@ from src.external_api import convertion_currency
 #     response = requests.get(f'https://api.github.com/users/{username}')
 #     return response.json()
 
+# @patch('requests.get')
+# def test_get_convertion_currency(mock_get):
+#     mock_get.return_value.json.return_value = {'login': 'testuser', 'name': 'Test User'}
+#     assert get_github_user_info('testuser') == {'login': 'testuser', 'name': 'Test User'}
+#     mock_get.assert_called_once_with('https://api.github.com/users/testuser')
+
 @patch('requests.get')
 def test_get_convertion_currency(mock_get):
-    mock_get.return_value.json.return_value = {'login': 'testuser', 'name': 'Test User'}
-    assert get_github_user_info('testuser') == {'login': 'testuser', 'name': 'Test User'}
-    mock_get.assert_called_once_with('https://api.github.com/users/testuser')
+    mock_get.return_value.json.return_value = {"result": "89.340238" }
+    assert convertion_currency("1.0", "USD") ==
+
